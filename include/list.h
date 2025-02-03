80d0ed28101685cb1e68bb7db0ea283ba9c7cbd8
allan <allan@cs.ucr.edu>
2025-02-02 22:07:03 -0800

    first commit

---
+++ include/list.h
@@ -0,0 +1,122 @@
+#include <initializer_list>
+#include <iostream>
+
+namespace engr101 {
+    template <class T> class list {
+        protected:
+        class node {
+            public:
+            node(const T &t, node *f = nullptr, node *b = nullptr): data(t), forward(f), backward(b) {}
+            T data;
+            node *forward;
+            node *backward;
+        };
+
+        private:
+        node *head;
+        node *tail;
+
+        public:
+        list(): head(nullptr), tail(nullptr) {}
+        list(std::initializer_list<T> l) : head(nullptr), tail(nullptr) {
+            for(auto i = l.begin(); i != l.end(); ++i) {
+                push_back(*i);
+            }            
+        }
+
+        public:
+        class iterator {
+            using value_type = T;
+            using pointer = T*;
+            using reference = T&;
+
+            node *n;
+            public:
+            iterator(node *n): n(n) {}
+            iterator &operator++() { n = n->forward; return *this; }
+            iterator operator++(int) { iterator temp = *this; n = n.forward; return temp; }
+
+            bool operator==(const iterator &b) { return n == b.n; }
+            bool operator!=(const iterator &b) { return n != b.n; }
+
+            reference operator*() { return n->data; }
+        };
+
+        void push_back(const T &t) {
+            if (head == nullptr && tail == nullptr) {
+                head = tail = new node(t, nullptr, nullptr);
+            } else {
+                node *appended = new node(t, nullptr, tail);
+                tail->forward = appended;
+                tail = appended;
+            }        
+        }
+
+        void reverse() {
+            // put your code here
+        }
+
+        iterator begin() { return iterator(head); }
+        iterator end() { return iterator(nullptr); }
+    };
+
+    template <class T> class forward_list {
+        protected:
+        class node {
+            public:
+            node(const T& t, node *f = nullptr): data(t), forward(f) {}
+            T data;
+            node *forward;
+        };
+
+        private:
+        node *head;
+        node *tail;
+
+        public:
+        forward_list(): head(nullptr), tail(nullptr) {}
+        forward_list(std::initializer_list<T> l): head(nullptr), tail(nullptr) {
+            for (auto i = l.begin(); i != l.end(); ++i) {
+                push_front(*i);
+            }
+        }
+
+        public: 
+        public:
+        class iterator {
+            using value_type = T;
+            using pointer = T*;
+            using reference = T&;
+
+            node *n;
+            public:
+            iterator(node *n): n(n) {}
+            iterator &operator++() { n = n->forward; return *this; }
+            iterator operator++(int) { iterator temp = *this; n = n->forward; return temp; }
+
+            bool operator==(const iterator &b) { return n == b.n; }
+            bool operator!=(const iterator &b) { return n != b.n; }
+
+            reference operator*() { return n->data; }
+        };
+
+        public:
+        void push_front(const T &t) {
+            if (head == nullptr && tail == nullptr) {
+                head = tail = new node(t, nullptr);
+            } else {
+                node *appended = new node(t, head);
+                head = appended;
+            }
+        }
+
+        void reverse() {
+            // put your code here
+        }
+
+        iterator begin() { return iterator(head); }
+        iterator end() { return iterator(nullptr); }
+    };
+}
+
+
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "list.h"
+#include <algorithm>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    engr101::list<int> l {1, 3, 5, 7, 9};
+    cout << "Doubly linked list before reverse" << endl;
+    std::for_each(l.begin(), l.end(), [](int i) { cout << i << endl; });
+
+    l.reverse();
+    cout << "Doubly linked list after reverse" << endl;
+    std::for_each(l.begin(), l.end(), [](int i) { cout << i << endl; });
+
+    engr101::forward_list<int> fl {0, 2, 4, 6, 8};
+    cout << "Singly linked list before reverse: " << endl;
+    std::for_each(fl.begin(), fl.end(), [](int i) { cout << i << endl; });
+
+    fl.reverse();
+    cout << "Singly linked list after reverse: " << endl;
+    std::for_each(fl.begin(), fl.end(), [](int i) { cout << i << endl; });
+
+    // Test that empty and single elements work
+    engr101::list<int> l2;
+    l2.reverse();
+    cout << "Empty doubly linked list after reverse" << endl;
+    std::for_each(l2.begin(), l2.end(), [](int i) { cout << i << endl; });
+    l2.push_back(1);
+    l2.reverse();
+    cout << "Single item doubly linked list after reverse" << endl;
+    std::for_each(l2.begin(), l2.end(), [](int i) { cout << i << endl; });
+    
+    engr101::forward_list<int> fl2;
+    fl2.reverse();
+    cout << "Empty singly linked list after reverse" << endl;
+    std::for_each(fl2.begin(), fl2.end(), [](int i) { cout << i << endl; });
+    fl2.push_front(1);
+    fl2.reverse();
+    cout << "Single item singly linked list after reverse" << endl;
+    std::for_each(fl2.begin(), fl2.end(), [](int i) { cout << i << endl; });
+
+    return 0;
+}
