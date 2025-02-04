2d98d9c2e6d8da0c6dca49fde863d6122a7d338b
Allan Knight <67027104+ucrdrk@users.noreply.github.com>
2025-02-04 00:33:05 +0000

    OCD fixes of colons in strings

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ using std::endl;
 
 int main() {
     engr101::list<int> l {1, 3, 5, 7, 9};
-    cout << "Doubly linked list before reverse" << endl;
+    cout << "Doubly linked list before reverse:" << endl;
     std::for_each(l.begin(), l.end(), [](int i) { cout << i << endl; });
 
     l.reverse();
-    cout << "Doubly linked list after reverse" << endl;
+    cout << "Doubly linked list after reverse:" << endl;
     std::for_each(l.begin(), l.end(), [](int i) { cout << i << endl; });
 
     engr101::forward_list<int> fl {0, 2, 4, 6, 8};
@@ -25,20 +25,20 @@ int main() {
     // Test that empty and single elements work
     engr101::list<int> l2;
     l2.reverse();
-    cout << "Empty doubly linked list after reverse" << endl;
+    cout << "Empty doubly linked list after reverse:" << endl;
     std::for_each(l2.begin(), l2.end(), [](int i) { cout << i << endl; });
     l2.push_back(1);
     l2.reverse();
-    cout << "Single item doubly linked list after reverse" << endl;
+    cout << "Single item doubly linked list after reverse:" << endl;
     std::for_each(l2.begin(), l2.end(), [](int i) { cout << i << endl; });
     
     engr101::forward_list<int> fl2;
     fl2.reverse();
-    cout << "Empty singly linked list after reverse" << endl;
+    cout << "Empty singly linked list after reverse:" << endl;
     std::for_each(fl2.begin(), fl2.end(), [](int i) { cout << i << endl; });
     fl2.push_back(1);
     fl2.reverse();
-    cout << "Single item singly linked list after reverse" << endl;
+    cout << "Single item singly linked list after reverse:" << endl;
     std::for_each(fl2.begin(), fl2.end(), [](int i) { cout << i << endl; });
 
     return 0;
